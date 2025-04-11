01ab9feae9e41869b477c1e2bb40f3aa3f9f2c04
Ivan Hribernik <cihribernik@gmail.com>
2025-04-11 12:58:50 -0300

    feat: finished day03 part 1

--- internal/solutions/y2015/day03.go
+++ internal/solutions/y2015/day03.go
@@ -1,6 +1,7 @@
 package y2015
 
 import (
+	"slices"
 	"strings"
 
 	"github.com/ihribernik/advent-of-code/internal/solutions"
@@ -8,6 +9,11 @@ import (
 )
 
 type Day03 struct{}
+
+func (d Day03) parseLine(input []string) []string {
+	inputVars := strings.Split(input[0], "")
+	return inputVars
+}
 
 func (d Day03) SolvePart1(input []string) (int, error) {
 	position := common.Direction{X: 0, Y: 0}
@@ -26,13 +32,23 @@ func (d Day03) SolvePart1(input []string) (int, error) {
 	return len(visited), nil
 }
 
-func (d Day03) parseLine(input []string) []string {
-	inputVars := strings.Split(input[0], "")
-	return inputVars
-}
-
 func (d Day03) SolvePart2(input []string) (int, error) {
-	panic("unimplemented")
+	position := common.Direction{X: 0, Y: 0}
+
+	visited := map[string]bool{
+		position.String(): true,
+	}
+	directions := strings.Split(input[0], "")
+
+	for chunk := range slices.Chunk(directions, 2) {
+		for _, direction := range chunk {
+			println(direction)
+			position := common.DIRECTIONS[direction].NewPosition(position, 1)
+			visited[position.String()] = true
+		}
+
+	}
+	return len(visited), nil
 }
 
 func init() {
